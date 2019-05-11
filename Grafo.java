276105582cb351437ec7e407dc1373e98e001ca3
OtavioLRS <otavioleitesantos@hotmail.com>
2019-05-11 01:25:15 -0300

    add TOC_fix

--- Grafo.java
+++ Grafo.java
@@ -3,6 +3,7 @@ package main;
 import java.util.ArrayList;
 
 public class Grafo {
+    
     private char marcador;
     private int chave;
     private int predecessor;
@@ -10,67 +11,67 @@ public class Grafo {
     private int tempoChegada;
     private int tempoFim;	
 	
-	public int getChave() {
-            return chave;
-	}
+    public int getMarcador(){
+        return marcador;
+    }
 	
-	public void setChave(int chave) {
-            this.chave = chave;
-	}
+    public void setMarcador(char marcador) {
+        this.marcador = marcador;
+    }
+    
+    public int getChave() {
+        return chave;
+    }
+	
+    public void setChave(int chave) {
+        this.chave = chave;
+    }
+    
+    public int getPredecessor() {
+        return predecessor;
+    }
+
+    public void setPredecessor(int predecessor) {
+        this.predecessor = predecessor;
+    }
 		
-	public int getDistancia() {
-            return distancia;
-	}
+    public int getDistancia() {
+        return distancia;
+    }
 
-	public void setDistancia(int distancia) {
-            this.distancia = distancia;
-	}
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
 
-	public int getPredecessor() {
-            return predecessor;
-	}
+    public int getTempoChegada() {
+        return tempoChegada;
+    }
 
-	public void setPredecessor(int predecessor) {
-            this.predecessor = predecessor;
-	}
+    public void setTempoChegada(int tempoChegada) {
+        this.tempoChegada = tempoChegada;
+    }
 
-	public int getMarcador(){
-            return marcador;
-	}
+    public int getTempoFim() {
+        return tempoFim;
+    }
+
+    public void setTempoFim(int tempoFim) {
+        this.tempoFim = tempoFim;
+    }
+
+    public Grafo(int chave) {
+        this.marcador = 'b';
+        this.distancia = -1;
+        this.predecessor = -1;
+        this.tempoChegada = 0;
+        this.tempoFim = 0;
+        this.chave = chave;
+    }
 	
-	public void setMarcador(char marcador) {
-            this.marcador = marcador;
-	}
-	
-	public int getTempoChegada() {
-            return tempoChegada;
-	}
-
-	public void setTempoChegada(int tempoChegada) {
-            this.tempoChegada = tempoChegada;
-	}
-
-	public int getTempoFim() {
-            return tempoFim;
-	}
-
-	public void setTempoFim(int tempoFim) {
-            this.tempoFim = tempoFim;
-	}
-
-	public Grafo(int chave) {
-            this.marcador = 'b';
-            this.distancia = -1;
-            this.predecessor = -1;
-            this.tempoChegada = 0;
-            this.tempoFim = 0;
-            this.chave = chave;
-	}
-	
+    /* Metodo pertencente a busca de profundidade com lista de adjacencia */
     public int visita(Grafo grafos[], int tempo, ArrayList<Aresta> arestas[]){
         this.setMarcador('c');
         this.setTempoChegada(tempo++);
-        //System.out.println("Cheguei no vertice "+this.chave+" no tempo "+this.tempoChegada);
         int prox = -1;
         for(int i=0;i<arestas[this.getChave()].size();i++){
             prox = arestas[this.getChave()].get(i).getChave();
@@ -81,15 +82,13 @@ public class Grafo {
         }
         this.setMarcador('p');
         this.setTempoFim(tempo++);
-        //System.out.println("Saida do vertice "+this.chave+" no tempo "+this.tempoFim);
         return tempo;
     }
     
+    /* Metodo pertencente a busca de profundidade com matriz de adjacencia */
     public int visita(Grafo grafos[], int tempo, int arestas[][]){
         this.setMarcador('c');
         this.setTempoChegada(tempo++);
-        //System.out.println("Cheguei no vertice "+this.chave+" no tempo "+this.tempoChegada);
-
         for(int i=0;i<arestas.length;i++){
             if(arestas[this.getChave()][i] != 0){
                 if(grafos[i].getMarcador() == 'b'){
@@ -100,9 +99,14 @@ public class Grafo {
         }
         this.setMarcador('p');
         this.setTempoFim(tempo++);
-        //System.out.println("Saida do vertice "+this.chave+" no tempo "+this.tempoFim);
         return tempo;
     }
 
-	
+    /* Metodo pertencente ao algoritmo Prim */
+    public boolean pertence(ArrayList<Grafo> lista){
+        for(int i=0; i<lista.size(); i++)
+            if (lista.get(i).getChave() == this.chave)
+                return true;
+        return false;
+    }
 }
